e3cfe2dbfe0f9f99114c0571ba978e6356a48f87
gucheng <1806909741@qq.com>
2021-12-10 14:59:50 +0800

    test

---
+++ cache/build/CMakeFiles/cache_server.out.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/test/dc/LittlePenguin/cache/src/CacheServer.cpp" "CMakeFiles/cache_server.out.dir/src/CacheServer.cpp.o" "gcc" "CMakeFiles/cache_server.out.dir/src/CacheServer.cpp.o.d"
+  "/home/test/dc/LittlePenguin/cache/src/HashSlot.cc" "CMakeFiles/cache_server.out.dir/src/HashSlot.cc.o" "gcc" "CMakeFiles/cache_server.out.dir/src/HashSlot.cc.o.d"
+  "/home/test/dc/LittlePenguin/cache/src/LRUCache.cpp" "CMakeFiles/cache_server.out.dir/src/LRUCache.cpp.o" "gcc" "CMakeFiles/cache_server.out.dir/src/LRUCache.cpp.o.d"
+  "/home/test/dc/LittlePenguin/cache/src/TcpServer.cpp" "CMakeFiles/cache_server.out.dir/src/TcpServer.cpp.o" "gcc" "CMakeFiles/cache_server.out.dir/src/TcpServer.cpp.o.d"
+  "/home/test/dc/LittlePenguin/cache/src/TcpSocket.cpp" "CMakeFiles/cache_server.out.dir/src/TcpSocket.cpp.o" "gcc" "CMakeFiles/cache_server.out.dir/src/TcpSocket.cpp.o.d"
+  "/home/test/dc/LittlePenguin/cache/src/cache_main.cpp" "CMakeFiles/cache_server.out.dir/src/cache_main.cpp.o" "gcc" "CMakeFiles/cache_server.out.dir/src/cache_main.cpp.o.d"
+  "/home/test/dc/LittlePenguin/cache/src/cmcdata.pb.cc" "CMakeFiles/cache_server.out.dir/src/cmcdata.pb.cc.o" "gcc" "CMakeFiles/cache_server.out.dir/src/cmcdata.pb.cc.o.d"
+  "/home/test/dc/LittlePenguin/cache/src/crc16.cpp" "CMakeFiles/cache_server.out.dir/src/crc16.cpp.o" "gcc" "CMakeFiles/cache_server.out.dir/src/crc16.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cache/build/CMakeFiles/cache_server.out.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cache_server.out.
+# This may be replaced when dependencies are built.
