d5d7fc8eb6a34eb36f0d59c328696376539a7874
Pancake-s <simonsong512@gmail.com>
2021-12-11 10:05:11 +0800

    对CacheNode.h增加了2个subscript operator，用于访问/修改哈希槽数据；添加了类公共成员的注释说明

--- master/include/CacheNode.h
+++ master/include/CacheNode.h
@@ -4,17 +4,18 @@
 #include <string>
 #include <bitset>
 
-#define HASHSLOT_SIZE 16384
+#define HASHSLOT_SIZE 16384     // Maximum number of cache nodes in the system
 
 class CacheNode {
     friend class HashSlot;
 public:
-    CacheNode(std::string ip, int p): ip_(ip), port_(p) { name_ = ip_ + std::to_string(port_); }
-    // CacheNode(std::string n, std::string ip, int p): name_(n), ip_(ip), port_(p) { }
-    std::string name() const { return name_; }
-    std::string ip() const { return ip_; }
-    int port() const { return port_; }
-    int numslots() const { return slots_.count(); }
+    CacheNode(std::string ip, int p): ip_(ip), port_(p) { name_ = ip_ + std::to_string(port_); }    // Usage: CacheNode("127.0.0.1", 5005);
+    std::string name() const { return name_; }          // returns the name of the cache node
+    std::string ip() const { return ip_; }              // returns the ip of the cache node
+    int port() const { return port_; }                  // returns the port of the cache node
+    int numslots() const { return slots_.count(); }     // returns the number of slots this cache node covers
+    constexpr bool operator[]( std::size_t pos ) const { return slots_[pos]; }                      // 返回pos处的哈希槽值0/1，Usage: cache_node[pos];
+    std::bitset<HASHSLOT_SIZE>::reference operator[]( std::size_t pos ) { return slots_[pos]; }     // 返回pos处哈希槽比特位的reference，Usage：cache_node[pos] = true/false;
 private:
     std::string name_;
     std::string ip_;
