51dfc200c9b3e7896c4455ee533f10a700d395b3
Pancake-s <simonsong512@gmail.com>
2021-12-11 11:09:47 +0800

    增加了对HashSlot中节点列表的访问

--- master/include/HashSlot.h
+++ master/include/HashSlot.h
@@ -13,24 +13,28 @@ class HashSlot {
 public:
     // Operations
     HashSlot() = default;
-    HashSlot(const HashSlot&);              // copy constructor - 节点变化时可能需要
-    HashSlot(const HashSlotInfo&);          // Build the HashSlot from a HashSlotInfo
+    HashSlot(const HashSlot&);              // copy constructor
+    HashSlot(const HashSlotInfo&);          // build the HashSlot from a HashSlotInfo
 
     HashSlot& operator=(const HashSlot&);   // assignment operator
-    void addCacheNode(const CacheNode&);
-    void remCacheNode(const CacheNode&);
+    void addCacheNode(const CacheNode&);    // add a cache node
+    void remCacheNode(const CacheNode&);    // remove a cache node
+    const CacheNode &cback() { return node_list_.back(); }                          // returns a reference to const to the last inserted cache node
+    std::list<CacheNode>::const_iterator cbegin() { return node_list_.cbegin(); }   // returns a const_iterator to the first cache node
+    std::list<CacheNode>::const_iterator cend() { return node_list_.cend(); }       // returns a const_iterator to the one-past-the-last cache node
+    int numNodes() { return num_nodes_; }   // returns the number of nodes in use
 
-    void saveTo(HashSlotInfo&) const;       // Save the HashSlot to a HashSlotInfo
-    void restoreFrom(const HashSlotInfo&);  // Restore from a HashSlotInfo
+    void saveTo(HashSlotInfo&) const;       // save the HashSlot to a HashSlotInfo
+    void restoreFrom(const HashSlotInfo&);  // restore hashslot from a HashSlotInfo
 
     void printCacheNode(std::ostream& os, int i) const { os << slots_[i]->name(); } 
 private:
-    int num_nodes_ = 0;                     // Num of cluster nodes
-    std::list<CacheNode> node_list_;        // List of all nodes in use
-    CacheNode *slots_[HASHSLOT_SIZE];       // Pointer to the Hash Slots
+    int num_nodes_ = 0;                     // num of nodes in use
+    std::list<CacheNode> node_list_;        // list of all nodes in use
+    CacheNode *slots_[HASHSLOT_SIZE];       // pointer to the Hash Slots
 };
 
 // Interface
-std::ostream& operator<<(std::ostream&, const HashSlot&);   // print node status
+std::ostream& operator<<(std::ostream&, const HashSlot&);   // print the node status
 
 #endif
