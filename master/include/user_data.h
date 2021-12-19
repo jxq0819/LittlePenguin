40f15e966fed18feade9801f5a9163276bd44da0
AC-xiaogu <72427458+AC-xiaogu@users.noreply.github.com>
2021-12-19 10:56:21 +0800

    Delete master directory

--- master/include/user_data.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-const unsigned int MAX_EVENTS = 200;
-const unsigned int MAX_BUFFER = 8192;
-const unsigned int MIN_PORT = 1024;
-const unsigned int MAX_PORT = 65535;
-const unsigned long LOG_SECONDS = 3600;
-
